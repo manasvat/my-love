6ae69ea9469a8d85293a7ea70411cf42c4c1b3d0
manasvat <88769677+manasvat@users.noreply.github.com>
2022-03-30 22:33:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);}
+
+  function draw (){
+    
+ 
+    if (keyIsDown(DOWN_ARROW))
+    {
+     background('red');
+   
+   }
+
+  
+   if (keyIsDown(UP_ARROW))
+   {
+    background('blue');
+  
+  }
+
+  
+  if (keyIsDown(RIGHT_ARROW))
+    {
+     background('yellow');
+   
+   }
+
+
+
+
+   if (keyIsDown(LEFT_ARROW))
+    {
+     background('ORANGE');
+   
+   }
+  
+  
+  
+  
+  
+  }
